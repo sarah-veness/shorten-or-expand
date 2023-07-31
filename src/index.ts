331e9ca2ad3532c009e06009f2506babd09ed7a7
Sarah Veness <sarahhveness@gmail.com>
2023-07-31 15:25:19 -0700

    rename dist -> lib

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+const { Command } = require('commander');
+
+const program = new Command
+
+program
+  .version('1.0.0')
+  .description('A CLI for shortening and expanding URLs')
+  .option('-s, --urlToShorten <value>', 'shorten the specified URL')
+  .option('-e, --urlToExpand <value>', 'expand the specified URL')
+  .parse(process.argv)
+
+const userInput = program.opts();
+
+if (userInput.urlToExpand) {
+  console.log(`this is the url to expand: ${userInput.urlToExpand}`)
+} else if (userInput.urlToShorten) {
+  console.log(`this is the url to shorten: ${userInput.urlToShorten}`)
+} else {
+  program.outputHelp();
+}
