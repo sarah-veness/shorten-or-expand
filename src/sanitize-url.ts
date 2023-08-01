921d583261e2ee0d39e98445b18ec41ca4a4a624
Sarah Veness <sarahhveness@gmail.com>
2023-08-01 00:14:15 -0700

    add new strip url method

--- src/sanitize-url.ts
+++ src/sanitize-url.ts
@@ -1,6 +1,18 @@
-export default async function sanitizeUrl(url: string) {
+export async function sanitizeUrl(url: string) {
   if (!(/^[^:]+(?=:\/\/)/).test(url)) {
     return "https://" + url;
+  } else {
+    return url;
+  }
+};
+
+export async function stripUrl(url: string) {
+  if ((/^[^:]+(?=:\/\/)/).test(url)) {
+    let strippedUrl = url.split('//')
+    return strippedUrl[1]
+  } else if ((/www./).test(url)) {
+    let strippedUrl = url.split('www.')
+    return strippedUrl[1]
   } else {
     return url;
   }
--- test/sanitize-url.test.js
+++ test/sanitize-url.test.js
@@ -1,7 +1,7 @@
 import { describe, expect, test } from '@jest/globals';
-import sanitizeUrl from '../src/sanitize-url'
+import { sanitizeUrl, stripUrl } from '../src/sanitize-url'
 
-describe('Sanitize URL', () => {
+describe('Sanitize', () => {
   test('returns the URL if it begins with http://', async () => {
     let url = await sanitizeUrl('http://test.com')
     expect(url).toBe('http://test.com')
@@ -13,5 +13,24 @@ describe('Sanitize URL', () => {
   test('adds https:// if the provided url emits it', async () => {
     let url = await sanitizeUrl('test.com')
     expect(url).toBe('https://test.com')
+  })
+})
+
+describe('Strip', () => {
+  test('returns the stripped URL if it begins with http://', async () => {
+    let url = await stripUrl('http://bit.ly.com/sdasjd')
+    expect(url).toBe('bit.ly.com/sdasjd')
+  })
+  test('returns the stripped URL if it begins with https://', async () => {
+    let url = await stripUrl('https://bit.ly.com/sdasjd')
+    expect(url).toBe('bit.ly.com/sdasjd')
+  })
+  test('returns the stripped URL if it begins with www.', async () => {
+    let url = await stripUrl('www.bit.ly.com/sdasjd')
+    expect(url).toBe('bit.ly.com/sdasjd')
+  })
+  test('returns url if it does not include protocol', async () => {
+    let url = await stripUrl('bit.ly.com/sdasjd')
+    expect(url).toBe('bit.ly.com/sdasjd')
   })
 })
